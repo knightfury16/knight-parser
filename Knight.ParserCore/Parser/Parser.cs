44d4fd5cf474a5f3da8b5f3319261d47871ba763
knightfury16 <suhaibknight@gmail.com>
2025-06-30 18:59:05 +0600

    Defined grammar and parser body

--- Knight.ParserCore/Parser/Parser.cs
+++ Knight.ParserCore/Parser/Parser.cs
@@ -1,6 +1,35 @@
+using Knight.ParserCore.Parser.Node;
+
 namespace Knight.ParserCore.Parser;
+
+
+//lets define our grammar
+//
+//Template ::= (TextNode | KnightStatement | BlockStatement)*
+//KnightStatement ::= value
+//BlockStatement ::= Block
+//Block ::= (TextNode | KnightStatement | BlockStatement)*
+//TextNode ::= value
 
 public class Parser
 {
 
+    private List<Token> _tokens;
+
+
+    public Parser(List<Token> tokens)
+    {
+        _tokens = tokens;
+    }
+
+
+    public BodyNode Parse()
+    {
+
+        //i need a BlockStatmentParser
+        // a TextNodeParser
+        //a knightStatementParser
+
+    }
 }
+
