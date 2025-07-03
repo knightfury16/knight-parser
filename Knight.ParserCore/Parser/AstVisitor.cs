4acfd6d939bfd7e621e7b4b41f1acd1297c1188e
knightfury16 <suhaibknight@gmail.com>
2025-07-03 13:55:39 +0600

    Added AstVisitor stub

---
+++ Knight.ParserCore/Parser/AstVisitor.cs
@@ -0,0 +1,33 @@
+using Knight.ParserCore.Parser.Node;
+
+namespace Knight.ParserCore.Parser;
+
+
+public class AstVisitor
+{
+    public static void VisitAst(TemplateNode templateNode)
+    {
+
+    }
+
+    private void VisitBody(List<RootNode> body)
+    {
+
+    }
+
+    private void VisitTextNode(TextNode textNode)
+    {
+
+    }
+
+    private void VisitBlockStatement(BlockStatement blockStatement)
+    {
+
+    }
+
+    private void VisitKnightNode(KnightStatement knightStatement)
+    {
+
+    }
+
+}
