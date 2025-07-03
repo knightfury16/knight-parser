772dba29cb8b4f8af0f9eb2f8001fa729e7c1d81
knightfury16 <suhaibknight@gmail.com>
2025-07-03 14:28:54 +0600

    Ast visitor implemented

--- Knight.ParserCore/Parser/AstVisitor.cs
+++ Knight.ParserCore/Parser/AstVisitor.cs
@@ -3,31 +3,89 @@ using Knight.ParserCore.Parser.Node;
 namespace Knight.ParserCore.Parser;
 
 
+
 public class AstVisitor
 {
+    private static int _spacer = 0;
+
     public static void VisitAst(TemplateNode templateNode)
     {
+        Console.WriteLine("STARTING AST VISIT::\n\n");
+        Console.WriteLine("");
+        Console.WriteLine("Template Node: ");
+        var body = templateNode.Body;
+        if (body is null) throw new ArgumentNullException("Body of the template node found empty!");
+
+        VisitBody(body.ToList());
 
     }
 
-    private void VisitBody(List<RootNode> body)
+    private static void VisitBody(List<RootNode> body)
     {
 
+        foreach (var node in body)
+        {
+            switch (node)
+            {
+                case TextNode text:
+                    VisitTextNode(text);
+                    break;
+                case BlockStatement blockStatement:
+                    VisitBlockStatement(blockStatement);
+                    break;
+                case KnightStatement knight:
+                    VisitKnightNode(knight);
+                    break;
+                default:
+                    throw new Exception("Unexpected type found");
+            }
+        }
     }
 
-    private void VisitTextNode(TextNode textNode)
+    private static void VisitTextNode(TextNode textNode)
+    {
+        for (int i = 0; i < _spacer; i++)
+        {
+            Console.WriteLine("     ");
+        }
+        Console.WriteLine("Text Node: ");
+        Console.WriteLine(textNode.Text);
+    }
+
+    private static void VisitBlockStatement(BlockStatement blockStatement)
     {
 
+
+        for (int i = 0; i < _spacer; i++)
+        {
+            Console.WriteLine("     ");
+        }
+        Console.WriteLine("Block Statement: ");
+        Console.WriteLine($"Evaluator Name: {blockStatement.EvaluatorVariable.Name}");
+        Console.WriteLine($"Params: {blockStatement.Parameter?.FirstOrDefault()?.Name}");
+        Console.WriteLine("Consequent code: ");
+        _spacer++;
+        VisitBody(blockStatement.Consequent.Body.ToList());
+
+        if (blockStatement.Alternate is not null)
+        {
+            Console.WriteLine("Alternate code: ");
+            VisitBody(blockStatement.Alternate.Body.ToList());
+        }
     }
 
-    private void VisitBlockStatement(BlockStatement blockStatement)
+    private static void VisitKnightNode(KnightStatement knightStatement)
     {
 
+        for (int i = 0; i < _spacer; i++)
+        {
+            Console.WriteLine("     ");
+        }
+
+        Console.WriteLine("Knight Statement: ");
+        Console.WriteLine($"Evaluator Name: {knightStatement.EvaluatorVariable.Name}");
     }
 
-    private void VisitKnightNode(KnightStatement knightStatement)
-    {
 
-    }
 
 }
