e12a50dd7a05f7e31e8d63bc7852ddd68ab95896
knightfury16 <suhaibknight@gmail.com>
2025-07-07 15:38:55 +0600

    Added template keywords constant file

---
+++ Knight.ParserCore/Parser/Node/TemplateKeywords.cs
@@ -0,0 +1,10 @@
+namespace Knight.ParserCore.Parser;
+
+public static class TemplateKeywords
+{
+    public const string If = "if";
+    public const string Else = "else";
+    public const string EndIf = "endif";
+    public const string For = "for";
+    public const string EndFor = "endfor";
+}
