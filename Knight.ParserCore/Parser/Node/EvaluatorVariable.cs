30df14a45c7ca6f7710ebfcdcd80053583f00a8a
knightfury16 <suhaibknight@gmail.com>
2025-07-10 17:23:03 +0600

    Added evaluator enumerator property in EvaluatorVariable
    Evaluator enumerator can be null if it is not FOR statement. And default
    evalutor enumerator name of FOR statement is 'this'

--- Knight.ParserCore/Parser/Node/EvaluatorVariable.cs
+++ Knight.ParserCore/Parser/Node/EvaluatorVariable.cs
@@ -3,4 +3,5 @@ namespace Knight.ParserCore.Parser.Node;
 public class EvaluatorVariable(string name)
 {
     public string Name { get; set; } = name;
+    public string? EvalutorEnumerator { get; set; }
 }
