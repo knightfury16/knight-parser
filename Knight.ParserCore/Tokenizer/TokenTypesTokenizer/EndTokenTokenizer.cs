74b0e5113e944d92179ce2b017f0793d159a7f6d
knightfury16 <suhaibknight@gmail.com>
2025-08-11 22:32:03 +0600

    Implemented end token tokenzier

--- Knight.ParserCore/Tokenizer/TokenTypesTokenizer/EndTokenTokenizer.cs
+++ Knight.ParserCore/Tokenizer/TokenTypesTokenizer/EndTokenTokenizer.cs
@@ -1,3 +1,5 @@
+using Knight.ParserCore.Utils;
+
 namespace Knight.ParserCore.Tokenizer.TokenTypesTokenizer;
 
 
@@ -5,6 +7,25 @@ internal class EndTokenTokenizer : ITokenTypeTokenizer
 {
     public Token? Tokenzie(int node, ExtendedStringReader sourceReader)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(sourceReader);
+
+        if (node <= 0)
+        {
+            return null;
+        }
+
+        if (node.ToChar() != '}')
+        {
+            return null;
+        }
+
+        if (node.ToChar() == '}' && sourceReader.Peek().ToChar() == '}')
+        {
+            sourceReader.Read(); //pointer at second }
+            sourceReader.Read();// moving past the second }
+            return Token.EndExpression(sourceReader.GetContext());
+        }
+
+        return null;
     }
 }
